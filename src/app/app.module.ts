c94f3896235f952012b9f2e1c39a4296e98d2afe
grabu6 <mgrabulosa071@boscdelacoma.cat>
2022-12-20 17:29:37 +0100

    creacio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateDrivenComponent } from './projecte/components/template-driven/template-driven.component';
+import { ReactiveFormsComponent } from './projecte/components/reactive-forms/reactive-forms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateDrivenComponent,
+    ReactiveFormsComponent
   ],
   imports: [
     BrowserModule,
